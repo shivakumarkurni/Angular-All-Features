82c42ef58820431efde343600507591e4beb349f
Narayana <narayana.chinna22@gmail.com>
2017-05-22 16:57:26 +0530

    angular js features

---
+++ controller.js
@@ -0,0 +1,29 @@
+var app=angular.module("myApp",['ngRoute']);
+app.config(["$locationProvider",function($locationProvider){
+  $locationProvider.hashPrefix('')
+}])
+app.config(["$routeProvider",function($routeProvider){
+	$routeProvider.when("/",{
+		templateUrl:"pages/Features.html"
+	})
+	.when("/Employee",{
+		templateUrl:"pages/Employee.html"
+	})
+	.when("/Filters",{
+		templateUrl:"pages/Filters.html"
+	})
+}])
+app.controller("myController",["$scope",function($scope){
+      $scope.message="Angular JS All Features"
+}])
+
+app.controller("filterController",["$scope",function($scope){
+	var filter=[
+	            {name:"shiva",age:23,Gender:"Male",City:"Hyderabad"},
+	            {name:"Kumar",age:23,Gender:"Male",City:"Bangalore"},
+	            {name:"Arvind",age:25,Gender:"Male",City:"Chennai"},
+	            {name:"shiva",age:25,Gender:"Male",City:"Hyderabad"},
+                {name:"Rani",age:25,Gender:"Female",City:"Rajasthan"}
+	];
+	$scope.filters=filter
+}])
